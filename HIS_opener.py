83b1b457e2e1e6c60055d57a8ca9feeb3c384f03
Paul Spitzner <sitzkringel@web.de>
2019-12-31 17:51:20 +0100

    removed the main() function wrap in HIS_opener.py
    my version of fiji did not call main(), maybe the check ```if __name__ == '__main__':``` is never evaluated.
    
    fixed the problem for me

--- HIS_opener.py
+++ HIS_opener.py
@@ -27,12 +27,12 @@
 
 # Hamamatsu HIS ImageJ VirtualStack Opener
 # ----------------------------------------
-# This script quickly opens as a VirtualStack the typical HIS files generated with 
+# This script quickly opens as a VirtualStack the typical HIS files generated with
 # Hokawo and Hamamatsu Orca cameras (tested on Orca Flash 2.8 and 4.0 and Hokawo
 # versions 2.5 and 2.8). Note that if the number of frames is large the stack
 # might be corrupted towards the end of the file (you will see that if the image starts
 # to "travel"). This is due to a change in metadata length inside the HIS file and cannot
-# be easily avoided without a sequential opening. 
+# be easily avoided without a sequential opening.
 #
 
 from ij import IJ
@@ -48,128 +48,124 @@ from ij.gui import HTMLDialog
 import struct
 import os
 
-def main():
-  imp = IJ.getFilePath("Select HIS file")
-  if not imp:
-    return
-  root, ext = os.path.splitext(imp)
-  if ext.lower() != '.his':
-    cFrame = PlugInFrame('ERR DLG')
-    MessageDialog(cFrame, 'ERROR', 'Expected extension .his')
-    return
-
-  fID = open(imp, 'rb')
-  fID.seek(2)
-  offset = struct.unpack('<h', fID.read(2))[0]
-  fID.seek(14)
-  frames = struct.unpack('<I', fID.read(4))[0]
-  fID.seek(2)
-  commentBytes = struct.unpack('<h', fID.read(2))[0]
-  width = struct.unpack('<h', fID.read(2))[0]
-  height = struct.unpack('<h', fID.read(2))[0]
-  struct.unpack('<I', fID.read(4))[0]
-  fileType = struct.unpack('<h', fID.read(2))[0]
-  fID.read(50)
-
-  metadata = fID.read(commentBytes)
-  metadataSplit = metadata.split(';')
-  metadataStr = beginMetadata()
-  for it in metadataSplit:
-    sp = it.split("=")
-    if len(sp) > 1:
-      metadataStr += addMetadataEntry(sp[0], sp[1])
-  metadataStr += endMetadata()
-  metadataDlg = HTMLDialog("HIS metadata", metadataStr, 0)
-  size = metadataDlg.getSize()
-  if size.width < 300:
-    size.width = 300
-  if size.height < 500:
-    size.height = 500
-  metadataDlg.setSize(size)
-
-  # The second image metadata
-  # That's the gap (in case it is different)
-  fID.seek(offset+64+width*height*(fileType)+2)
-  gap = struct.unpack('<h', fID.read(2))[0]
-
-  # Now let's check the metadata size across several frames to see if it's consistent
-  vals = range(1, frames, int(frames/100))
-  md_old = gap
-  metadataInconsistency = 0
-  for it in range(0, len(vals)):
-    if metadataInconsistency > 0:
-      break
-    cFrame = vals[it]
-    if cFrame < 3:
-      continue
-    cPixel = (width*height*(fileType)+gap+64)*(cFrame-2)+offset+64+width*height*(fileType)+2
-    fID.seek(cPixel)
-    md_new = struct.unpack('<h', fID.read(2))[0]
-    if(md_new != md_old):
-      # Let's narrow the search
-      nvals = range(vals[it-1], vals[it]+1)
-      print nvals
-      for it2 in range(0, len(nvals)):
-        cFrame = nvals[it2]
-        if cFrame < 3:
-          continue
-        cPixel = (width*height*(fileType)+gap+64)*(cFrame-2)+offset+64+width*height*(fileType)+2
-        fID.seek(cPixel)
-        md_new = struct.unpack('<h', fID.read(2))[0]
-        print md_new
-        if(md_new != md_old):
-          metadataInconsistency = cFrame
-          offset = cPixel+gap
-          gap = md_new
-          break
-        md_old = md_new
-    md_old = md_new
-
-  metadataStr = beginMetadata()
-  metadataStr += addMetadataEntry('width', str(width))
-  metadataStr += addMetadataEntry('height', str(height))
-  metadataStr += addMetadataEntry('nImages', str(frames))
-  metadataStr += addMetadataEntry('offset', str(offset+64))
-  metadataStr += addMetadataEntry('fileType', str(fileType))
-  metadataStr += addMetadataEntry('gapBetweenImages', str(gap+64))
-  if(metadataInconsistency > 0):
-    metadataStr += addMetadataEntry('Inconsistent METADATA', str(metadataInconsistency))
-  metadataStr += endMetadata()
-  metadataDlg = HTMLDialog("HIS parameters", metadataStr, 0)
-  size = metadataDlg.getSize()
-  if size.width < 300:
-    size.width = 300
-  if size.height < 300:
-    size.height = 300
-  metadataDlg.setSize(size)
-  
-  finfo = FileInfo()
-  finfo.fileName = imp
-  finfo.width = width
-  finfo.height = height
-  finfo.nImages = frames
-  finfo.offset = offset+64
-  finfo.fileType = fileType-1
-  finfo.intelByteOrder = 1
-  finfo.gapBetweenImages = gap+64
-  finfo.fileFormat = 1
-  finfo.samplesPerPixel = 1
-  finfo.displayRanges = None
-  finfo.lutSize = 0
-  finfo.whiteIsZero = 0
-  vs = VirtualStack()
-  finfo.virtualStack = vs
-  FileInfoVirtualStack(finfo)
-  
 def addMetadataEntry(name, val):
   return "<tr><td style='padding:0 25px 0 0px;'><b>" + name + "</b></td><td>" + val + "</td></tr>"
-  
+
 def beginMetadata():
   return "<table border=0 cellspacing=0>"
-  
+
 def endMetadata():
   return "</table>"
-  
 
-if __name__ == '__main__':
-  main()
+
+imp = IJ.getFilePath("Select HIS file")
+if not imp:
+  exit(-1)
+root, ext = os.path.splitext(imp)
+if ext.lower() != '.his':
+  cFrame = PlugInFrame('ERR DLG')
+  MessageDialog(cFrame, 'ERROR', 'Expected extension .his')
+  exit(-1)
+
+fID = open(imp, 'rb')
+fID.seek(2)
+offset = struct.unpack('<h', fID.read(2))[0]
+fID.seek(14)
+frames = struct.unpack('<I', fID.read(4))[0]
+fID.seek(2)
+commentBytes = struct.unpack('<h', fID.read(2))[0]
+width = struct.unpack('<h', fID.read(2))[0]
+height = struct.unpack('<h', fID.read(2))[0]
+struct.unpack('<I', fID.read(4))[0]
+fileType = struct.unpack('<h', fID.read(2))[0]
+fID.read(50)
+
+metadata = fID.read(commentBytes)
+metadataSplit = metadata.split(';')
+metadataStr = beginMetadata()
+for it in metadataSplit:
+  sp = it.split("=")
+  if len(sp) > 1:
+    metadataStr += addMetadataEntry(sp[0], sp[1])
+metadataStr += endMetadata()
+metadataDlg = HTMLDialog("HIS metadata", metadataStr, 0)
+size = metadataDlg.getSize()
+if size.width < 300:
+  size.width = 300
+if size.height < 500:
+  size.height = 500
+metadataDlg.setSize(size)
+
+# The second image metadata
+# That's the gap (in case it is different)
+fID.seek(offset+64+width*height*(fileType)+2)
+gap = struct.unpack('<h', fID.read(2))[0]
+
+# Now let's check the metadata size across several frames to see if it's consistent
+vals = range(1, frames, int(frames/100))
+md_old = gap
+metadataInconsistency = 0
+for it in range(0, len(vals)):
+  if metadataInconsistency > 0:
+    break
+  cFrame = vals[it]
+  if cFrame < 3:
+    continue
+  cPixel = (width*height*(fileType)+gap+64)*(cFrame-2)+offset+64+width*height*(fileType)+2
+  fID.seek(cPixel)
+  md_new = struct.unpack('<h', fID.read(2))[0]
+  if(md_new != md_old):
+    # Let's narrow the search
+    nvals = range(vals[it-1], vals[it]+1)
+    print nvals
+    for it2 in range(0, len(nvals)):
+      cFrame = nvals[it2]
+      if cFrame < 3:
+        continue
+      cPixel = (width*height*(fileType)+gap+64)*(cFrame-2)+offset+64+width*height*(fileType)+2
+      fID.seek(cPixel)
+      md_new = struct.unpack('<h', fID.read(2))[0]
+      print md_new
+      if(md_new != md_old):
+        metadataInconsistency = cFrame
+        offset = cPixel+gap
+        gap = md_new
+        break
+      md_old = md_new
+  md_old = md_new
+
+metadataStr = beginMetadata()
+metadataStr += addMetadataEntry('width', str(width))
+metadataStr += addMetadataEntry('height', str(height))
+metadataStr += addMetadataEntry('nImages', str(frames))
+metadataStr += addMetadataEntry('offset', str(offset+64))
+metadataStr += addMetadataEntry('fileType', str(fileType))
+metadataStr += addMetadataEntry('gapBetweenImages', str(gap+64))
+if(metadataInconsistency > 0):
+  metadataStr += addMetadataEntry('Inconsistent METADATA', str(metadataInconsistency))
+metadataStr += endMetadata()
+metadataDlg = HTMLDialog("HIS parameters", metadataStr, 0)
+size = metadataDlg.getSize()
+if size.width < 300:
+  size.width = 300
+if size.height < 300:
+  size.height = 300
+metadataDlg.setSize(size)
+
+finfo = FileInfo()
+finfo.fileName = imp
+finfo.width = width
+finfo.height = height
+finfo.nImages = frames
+finfo.offset = offset+64
+finfo.fileType = fileType-1
+finfo.intelByteOrder = 1
+finfo.gapBetweenImages = gap+64
+finfo.fileFormat = 1
+finfo.samplesPerPixel = 1
+finfo.displayRanges = None
+finfo.lutSize = 0
+finfo.whiteIsZero = 0
+vs = VirtualStack()
+finfo.virtualStack = vs
+FileInfoVirtualStack(finfo)
